fde1dd2416b6ec6924eaa04c717474a128d9f81f
Karel Smutný <karel.smutny@outlook.com>
2025-03-26 20:30:07 +0000

    refactor: Generalized quiz score assert step

--- frontend/specs/steps/quiz-score.ts
+++ frontend/specs/steps/quiz-score.ts
@@ -5,30 +5,24 @@ Given('I finish the quiz', async function () {
     await this.page.goto('quiz/score')
 })
 
-Then('I see the result 2 correct out of 2, 100%, passed', async function () {
-    const correctAnswers = await this.quizScorePage.correctAnswers()
-    expect(correctAnswers).toBe(2)
+Then(
+    /^I see the result (\d+) correct out of (\d+), (\d+)%, (passed|failed)/,
+    async function (
+        expectedCorrectAnswers: number,
+        expectedTotalQuestions: number,
+        expectedPercentage: number,
+        expectedTextResult: string,
+    ) {
+        const correctAnswers = await this.quizScorePage.correctAnswers()
+        expect(correctAnswers).toBe(expectedCorrectAnswers)
 
-    const totalQuestions = await this.quizScorePage.totalQuestions()
-    expect(totalQuestions).toBe(2)
+        const totalQuestions = await this.quizScorePage.totalQuestions()
+        expect(totalQuestions).toBe(expectedTotalQuestions)
 
-    const result = await this.quizScorePage.percentageResult()
-    expect(result).toBe(100.0)
+        const result = await this.quizScorePage.percentageResult()
+        expect(result).toBe(expectedPercentage)
 
-    const textResult = await this.quizScorePage.textResult()
-    expect(textResult).toBe('passed')
-})
-
-Then('I see the result 1 correct out of 2, 50%, failed', async function () {
-    const correctAnswers = await this.quizScorePage.correctAnswers()
-    expect(correctAnswers).toBe(1)
-
-    const totalQuestions = await this.quizScorePage.totalQuestions()
-    expect(totalQuestions).toBe(2)
-
-    const result = await this.quizScorePage.percentageResult()
-    expect(result).toBe(50.0)
-
-    const textResult = await this.quizScorePage.textResult()
-    expect(textResult).toBe('failed')
-})
+        const textResult = await this.quizScorePage.textResult()
+        expect(textResult).toBe(expectedTextResult)
+    },
+)
