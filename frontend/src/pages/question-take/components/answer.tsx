af5154d01adbde20816b825b63f475bb099edfae
Michal Kazda <113020124+michalkazda-ext54850@users.noreply.github.com>
2025-05-06 15:42:40 +0200

    fix: linting

--- frontend/src/pages/question-take/components/answer.tsx
+++ frontend/src/pages/question-take/components/answer.tsx
@@ -23,9 +23,7 @@ export const Answer = (props: AnswerProps) => {
     return (
         <li data-testid={`answer-row-${props.answer}`} key={props.idx}>
             <input type={checkType} name={checkName} id={answerId} value={props.answer} onChange={onChange} />
-            <label htmlFor={answerId}>
-                {props.answer}
-            </label>
+            <label htmlFor={answerId}>{props.answer}</label>
             <br />
             <br />
         </li>
