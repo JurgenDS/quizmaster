23cc17f88102a9774d0d782d3c682d3a039728c8
Artem Z. <107121930+ArtemUnreal71@users.noreply.github.com>
2025-06-18 11:16:45 +0200

    Fix formatovani

--- frontend/src/pages/create-question-list/form/title-edit.tsx
+++ frontend/src/pages/create-question-list/form/title-edit.tsx
@@ -6,6 +6,12 @@ interface TitleEditProps {
 export const TitleEdit = ({ title, setTitle }: TitleEditProps) => (
     <>
         <label htmlFor="question-text">Enter list title:</label>
-        <textarea id="question-list-title" data-testId="question-list-title" value={title} onChange={e => setTitle(e.target.value)} rows={1} />
+        <textarea
+            id="question-list-title"
+            data-testId="question-list-title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            rows={1}
+        />
     </>
 )
