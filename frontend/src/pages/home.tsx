b208f36e886ba85222a2148445ec6159704f0629
JurgenDS <jurgends@gmail.com>
2025-08-08 16:06:07 +0200

    Changed title as test, added "your rock" at the end

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export const HomePage = () => (
     <>
-        <h1>Welcome to Quizmaster!</h1>
+        <h1>Welcome to Quizmaster! You rock.</h1>
         <Link to="/question/new">Create new question</Link>
         <br />
         <Link to="/q-list/new">Create new question list</Link>
